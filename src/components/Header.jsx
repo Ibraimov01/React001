530e6a6fe11b757ba8455485013ac61ee5fdbafe
Ibraimov01 <99306331+Ibraimov01@users.noreply.github.com>
2022-10-29 07:28:56 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return(
+<header className="header">
+  <img src="https://www.logodesign.net/logo/crossed-steel-pillars-2472ld.png" alt="" />
+</header>
+  )
+}
+
+ export default Header;
