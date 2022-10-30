45feadda92d3f563aa7b71993451988855e1d1c9
Ibraimov01 <99306331+Ibraimov01@users.noreply.github.com>
2022-10-31 00:02:43 +0600

    commit

---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import s from './MyPosts.module.css';
+import Post from "./Post/Post";
+
+
+
+const MyPosts = () => {
+  return(
+  <div>
+    my posts
+    <div>
+      <textarea></textarea>
+      <button>Add post</button>
+    
+  </div>
+
+  <div className={s.posts}>
+    <Post message='Hi, how are u?' />
+    <Post message="It's my first post"/>
+    
+  </div>
+ </div>
+  )
+}
+
+ export default MyPosts;
